4dd8966b6a3a57e3ac39fc42a331af22a94133c4
MEd-Douiri <mr.mouad.ed.douiri@gmail.com>
2022-08-22 09:37:58 +0200

    updates

--- flutter_stable_sdk_version.py
+++ flutter_stable_sdk_version.py
@@ -4,9 +4,17 @@
 
 from urllib.request import urlopen
 import json
-
+ret = ""
 with urlopen('https://storage.googleapis.com/flutter_infra_release/releases/releases_linux.json') as f:
         releases_linux_raw = f.read().decode('utf-8').strip()
         releases_linux_json = json.loads(releases_linux_raw)
         flutter_sdk_tag = releases_linux_json["current_release"]["stable"]
-        print(flutter_sdk_tag)
+        releases = releases_linux_json["releases"]
+        for r in releases:
+            if r["version"] == flutter_sdk_tag or r["hash"] == flutter_sdk_tag:
+                ret = r["version"]
+                print(ret)
+                break
+if ret == "":
+        raise ValueError("Could not get flutter sdk archive url")
+
